a9a1effbc9e1cf9ffec9169f445c7d27971a7fb4
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-27 17:37:16 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -3,18 +3,23 @@ variable "s3_origin_id" {
   type = string
 
 }
-/*
-variable "admin_domain_name" {
-  default = "admin domain name for cloud front"
+
+variable "origin_id" {
+  default = "origin id for cloud front"
   type = string
 }
-*/
+
 variable "env" {
   default = "environment of cloud front distribution"
   type = string  
 }
-/*
+
 variable "domain_name" {
   description = "domain name for CF"
   type = string
-}*/
+}
+
+variable "target_origin_id" {
+  description = "domain name for CF"
+  type = string
+}
