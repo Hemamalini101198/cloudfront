1315ce70073f149f99d07ac1460a146f3c95d964
Hemamalini101198 <150349833+Hemamalini101198@users.noreply.github.com>
2023-11-29 19:29:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,15 @@
+data "aws_cloudfront_origin_request_policy" "this" {
+  name = "Managed-CORS-S3Origin"
+}
+
+data "aws_cloudfront_cache_policy" "this" {
+  name = "Managed-CachingOptimized"
+}
+
+data "aws_cloudfront_response_headers_policy" "this"{
+  name = "CorsFullAccess"
+}
+
 resource "aws_cloudfront_distribution" "cf_s3_distribution" {
   origin {
     domain_name = var.domain_name
@@ -11,42 +23,18 @@ enabled             = true
 default_cache_behavior {
    # viewer_protocol_policy = "redirect-to-https"
     
-#  cache_policy_id              = "Managed-CachingOptimized"
-#  origin_request_policy_id     = "Managed-AllViewer"
+  cache_policy_id              = data.aws_cloudfront_cache_policy.this.id
+  origin_request_policy_id     = data.aws_cloudfront_origin_request_policy.this.id
+  response_headers_policy_id = data.aws_cloudfront_response_headers_policy.this.id
    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]          
-    target_origin_id = var.target_origin_id
-forwarded_values {
-      query_string = false
-cookies {
-        forward = "none"
-      }
-    }
+   cached_methods   = ["GET", "HEAD"]          
+   target_origin_id = var.target_origin_id
+
 viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
   }
- /* 
-  default_cache_behavior {
-    allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "test-bucket98548"
-forwarded_values {
-      query_string = false
-cookies {
-        forward = "none"
-      }
-    }
-
-viewer_protocol_policy = "redirect-to-https"
-   # min_ttl                = 0
-   # default_ttl            = 3600
-   # max_ttl                = 86400
-  # cache_policy_id              = aws_cloudfront_cache_policy.managed_caching_optimized.id
-  # origin_request_policy_id     = aws_cloudfront_origin_request_policy.managed_cors_s3_origin.id   
-  }
-  */
 
   price_class = "PriceClass_All"
 
